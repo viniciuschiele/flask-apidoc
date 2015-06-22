b1ec12b795bbd0145a9a2d70717ca0ae5c276d5c
viniciuschiele <vinicius.chiele@gmail.com>
2015-06-22 10:28:07 +0100

    Improved replacing of url

--- flask_apidoc/apidoc.py
+++ flask_apidoc/apidoc.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
 import mimetypes
 
 from flask import request
@@ -34,6 +35,7 @@ class ApiDoc(object):
             self.url_path = '/apidoc'
 
         self.app = None
+        self.__project_data = None
 
         if app:
             self.init_app(app)
@@ -48,25 +50,38 @@ class ApiDoc(object):
         app.add_url_rule(url, 'apidoc', self.__view)
         app.add_url_rule(url + '<path:path>', 'apidoc', self.__view)
 
+        self.__project_data = self.__load_project_info()
+
+    def __load_project_info(self):
+        file_name = join(self.app.static_folder, self.folder_path, 'api_project.json')
+
+        with open(file_name, 'rt') as file:
+            data = file.read()
+
+        return json.loads(data)
+
     def __view(self, path=None):
         if not path:
             path = 'index.html'
 
         file_name = join(self.folder_path, path)
 
-        if path == 'api_project.js' or path == 'api_project.json':
-            return self.__send_project_file(file_name)
+        if path == 'api_project.js' or path == 'api_data.js':
+            return self.__send_api_file(file_name)
 
         return self.app.send_static_file(file_name)
 
     @lru_cache(maxsize=1048576)
-    def __send_project_file(self, file_name):
+    def __send_api_file(self, file_name):
         file_name = join(self.app.static_folder, file_name)
 
         with open(file_name, 'rt') as file:
             data = file.read()
 
-        data = self.__replace_url(data)
+        self.__project_data.get('url').find('')
+
+        data = self.__replace_api_url(data, self.__project_data.get('url'))
+        data = self.__replace_api_url(data, self.__project_data.get('sampleUrl'))
 
         headers = Headers()
         headers['Content-Length'] = getsize(file_name)
@@ -81,17 +96,12 @@ class ApiDoc(object):
         return response
 
     @staticmethod
-    def __replace_url(data):
-        i = data.find('"url"')
+    def __replace_api_url(data, old_url):
+        i = old_url.find('/', 8)
 
-        if i == -1:
-            return data
+        if i > -1:
+            old_url = old_url[:i]
 
-        start = data.find('"', i + 5) + 1
-        end = data.find('"', start + 1)
+        new_url = request.url_root.strip('/')
 
-        content = data[:start]
-        content += request.url_root
-        content += data[end:]
-
-        return content
+        return data.replace(old_url, new_url)
