3fc5c3d32908f262befac3341d8eac7d137651b9
viniciuschiele <vinicius.chiele@gmail.com>
2016-03-21 10:06:06 +0000

    Adds new unit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,10 +3,20 @@ from flask_apidoc.utils import cached
 
 
 class TestUtils(TestCase):
-    def test_cache(self):
+    def test_cached_function(self):
         @cached
         def f(a, b):
             return object()
 
         self.assertEqual(f(1, 2), f(1, 2))
         self.assertNotEqual(f(1, 2), f(1, 3))
+
+    def test_cached_method(self):
+        class C(object):
+            @cached
+            def f(self, a, b):
+                return object()
+
+        c = C()
+        self.assertEqual(c.f(1, 2), c.f(1, 2))
+        self.assertNotEqual(c.f(1, 2), c.f(1, 3))
